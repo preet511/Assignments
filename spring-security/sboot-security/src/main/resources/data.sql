d4a52b8c5da2d19ccd73438ad3c024c8b3bb1a94
preet511 <73891944+preet511@users.noreply.github.com>
2021-06-15 10:36:45 +0530

    Delete spring-security/sboot-security directory

--- spring-security/sboot-security/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO springboot.app_user(id, username, password) VALUES (100,"ranjith","sekar");
-INSERT INTO springboot.app_user(id, username, password) VALUES (200,"spring","boot");
-INSERT INTO springboot.app_user(id, username, password) VALUES (300,"tom","cat");
--- spring-security/sboot-security/src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE springboot.app_user
-(
-   id INTEGER,
-   username VARCHAR(30),
-   password VARCHAR(30)
-);
